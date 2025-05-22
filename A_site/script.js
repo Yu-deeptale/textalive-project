01030e7a52605ea4dbc30052b5d2299dab981242
Yu-deeptale <wintinver@gmail.com>
2025-05-22 10:12:02 +0900

    base files

--- A_site/script.js
+++ A_site/script.js
@@ -1,1 +1,10 @@
+import { contentData } from "../content.js";
+
+const main = document.getElementById("content");
+
+contentData.forEach(({ title, text }) => {
+  const section = document.createElement("section");
+  section.innerHTML = `<h2>${title}</h2><p>${text}</p>`;
+  main.appendChild(section);
+});
 
